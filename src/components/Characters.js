6a3d4e19ee422802248b2173010cf77010561f85
AndrewPeralta11 <43342098+AndrewPeralta11@users.noreply.github.com>
2021-04-21 18:19:16 -0400

    made the characters data types/ displayed

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,10 +1,20 @@
 import React from "react";
+import data from "../data/character-info.json";
 
 function CharSelect() {
   return (
     <div>
       <h1>Adventure Game</h1>
       <h2>Choose your character</h2>
+      {data.map(function (character) {
+        return (
+          <div>
+            <h2> {character.name} </h2>
+            <img src={character.image} />
+            <h3> {character.description} </h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
