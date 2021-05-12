50a4e741f8b56cdc979fb93b66db5bc103ba4643
AndrewPeralta11 <43342098+AndrewPeralta11@users.noreply.github.com>
2021-05-12 18:39:12 -0400

    Finished dice roll mechanic key system was removed

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data/scenario-info.json";
 
-function renderScenario(scenario) {
-  return(
+function renderScenario(scenario, setsceneNumber, diceRoll) {
+  return (
     <div>
       <div className="tooltiptext"> {scenario.description} </div>
-        <img src={scenario.image}/>
-        <button> {scenario.optionA}</button>
-        <button> {scenario.optionB}</button>
+      <img src={scenario.image} />
+      <button onClick={() => diceRoll("A")}> {scenario.optionA} </button>
+      <button onClick={() => diceRoll("B")}> {scenario.optionB} </button>
     </div>
-  )
+  );
 }
 
 function Scenario() {
+  const [sceneNumber, setsceneNumber] = useState(0);
+  function diceRoll(optionPicked) {
+    let rollNum = Math.floor(Math.random() * 2);
+    if (rollNum % 2 === 0) {
+      let evenoption = `option${optionPicked}_even`;
+      setsceneNumber(data[sceneNumber][evenoption]);
+    } else {
+      let oddoption = `option${optionPicked}_odd`;
+      setsceneNumber(data[sceneNumber][oddoption]);
+    }
+    console.log(rollNum);
+  }
   return (
-    <div>{renderScenario(data[0])}</div>
-  )
+    <div>{renderScenario(data[sceneNumber], setsceneNumber, diceRoll)}</div>
+  );
 }
-
-
-
-
-  
 
 export default Scenario;
